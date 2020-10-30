f8ac190d1a7d345c5301942c9cfb8dd827f14986
SupitchayaTammy <73695883+SupitchayaTammy@users.noreply.github.com>
2020-10-30 17:50:19 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,25 @@
-<?php
-echo"<h2>PHP is Fun!<h/2>:;
-echo"Hello world<br>";
-?>
+<html>
+
+<head>
+    <title>Hello World</title>
+</head>
+
+<body>
+    <h1>Supitchaya</h1>
+    <p>Tammy</p>
+    <img src="ragdoll.jpg">
+    <img src="ragdoll2.jpg">
+    <img src="ragdoll3.jpg">
+    <br>
+    <a href="https://trupanion.com/breeds/cat/ragdoll">This is a link</a>
+
+    <h2>Jokes</h2>
+    <ul>
+        <li>เล่าเรื่องตลกไม่เป็นค่ะ</li>
+        <li>ทำไงดีล่ะคะ?</li>
+        <li>5555555555555555555555555555555555555555555</li>
+    </ul>
+
+</body>
+
+</html>
