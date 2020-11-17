35d4079e04e0c8cc880f40171bd39d8e2a04d28d
SupitchayaTammy <73695883+SupitchayaTammy@users.noreply.github.com>
2020-11-18 03:21:49 +0700

    Update delete.php

--- delete.php
+++ delete.php
@@ -6,8 +6,8 @@ if (mysqli_connect_errno($conn))
 {
     die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
-$name = $_REQUEST['name'];
-$sql = "DELETE FROM guestbook WHERE name="$name"";
+$Name = $_REQUEST['Name'];
+$sql = "DELETE FROM guestbook WHERE name="$Name"";
 if (mysqli_query($conn, $sql)) {
   echo "Delete successfully";
   echo '<a href="https://web247.azurewebsites.net/show.php">กลับสู่หน้าตาราง</a>';
