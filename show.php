39e117d97c4c2af8c7f21e2c8b6d3532127df854
SupitchayaTammy <73695883+SupitchayaTammy@users.noreply.github.com>
2020-11-18 02:15:30 +0700

    Update show.php

--- show.php
+++ show.php
@@ -12,7 +12,7 @@ if (mysqli_connect_errno($conn))
 }
 $res = mysqli_query($conn, 'SELECT * FROM guestbook');
 ?>
-<table width="600" border="1" style="color: white;margin: auto;font-size: large; font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;">
+<table width="600" border="1" style="color: white;margin: auto;font-size: large; font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;border-radius: 20px;border: none; box-shadow: none;overflow: visible;">
   <tr>
     <th width="100" style="background-color: rgb(136, 30, 56);"> <div align="center">Name</div></th>
     <th width="350" style="background-color:  rgb(136, 30, 56);"> <div align="center">Comment </div></th>
@@ -22,13 +22,13 @@ $res = mysqli_query($conn, 'SELECT * FROM guestbook');
 while($Result = mysqli_fetch_array($res))
 {
 ?>
-  <tr>
+  <tr &:nth-child(2n) style="background-color: white;" &:nth-child(2n+1) style="background-color: pink">
     <td><?php echo $Result['Name'];?></div></td>
     <td><?php echo $Result['Comment'];?></td>
     <td><form action = "delete.php" method = "post" id="CommentForm" style="text-align:left;">
-      <input type="submit" id="btn_del" value="ลบ" style="background-color:  rgb(136, 30, 56);border-radius: 5px;font-size: medium;color: white; height: 25px;border: none;width: 50px;">   
+      <input type="submit" id="btn_del" value="ลบ" style="background-color:  rgb(245, 100, 94);border-radius: 5px;font-size: medium;color: white; height: 25px;border: none;width: 50px;">   
     <form action = "edit.php" method = "post" id="CommentForm" style="text-align:center;">
-      <input type="submit" id="btn_del" value="แก้ไข" style="background-color:  rgb(136, 30, 56);border-radius: 5px;font-size: medium;color: white; height: 25px;border: none;width: 50px;">   
+      <input type="submit" id="btn_del" value="แก้ไข" style="color:  white;border-radius: 5px;font-size: medium;background-color:rgb(129, 190, 224); height: 25px;border: none;width: 50px;">   
     </form> 
   </tr>
 <?php
@@ -37,7 +37,7 @@ while($Result = mysqli_fetch_array($res))
 </table>
 <div class="container" style="text-align: center;padding: 2%;">
   <a href="https://web247.azurewebsites.net/form.html">
-    <button style="background-color:  rgb(136, 30, 56);border-radius: 5px;font-size: medium;color: white; height: 25px;border: none;width: 50px;">เพิ่ม</button>
+    <button style="background-color:  rgb(212, 74, 127);border-radius: 5px;font-size: medium;color: white; height: 25px;border: none;width: 50px;">เพิ่ม</button>
   </a>
 </div>
 <?php
