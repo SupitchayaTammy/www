a24e5d6543ff84491ae5f36c8c9df36b318e276f
SupitchayaTammy <73695883+SupitchayaTammy@users.noreply.github.com>
2020-11-18 02:39:38 +0700

    Add files via upload

---
+++ delete.php
@@ -0,0 +1,26 @@
+<?php
+
+$conn = mysqli_init();
+mysqli_real_connect($conn, 'supitchaya.mysql.database.azure.com', '_supitchaya_@supitchaya', 'Kmitlitf01', 'itflab', 3306);
+if (mysqli_connect_errno($conn))
+{
+    die('Failed to connect to MySQL: '.mysqli_connect_error());
+}
+
+
+$name = $_GET['name'];
+$comment = $_GET['comment'];
+
+
+
+$sql = "DELETE FROM show WHERE name="$name"";
+
+
+if (mysqli_query($conn, $sql)) {
+    header("Location: show.php");
+  } else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+  }
+  
+mysqli_close($conn);
+?>
