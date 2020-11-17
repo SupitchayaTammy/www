56c4a1ac0343f1c23b6f11de64d42626bb36b8fb
SupitchayaTammy <73695883+SupitchayaTammy@users.noreply.github.com>
2020-11-18 03:27:45 +0700

    Update show.php

--- show.php
+++ show.php
@@ -12,7 +12,7 @@ if (mysqli_connect_errno($conn))
 }
 $res = mysqli_query($conn, 'SELECT * FROM guestbook');
 ?>
-<table " width="600" border="1" style="background-color: white;margin: auto;color: rgb(136, 30, 56);font-size: large; font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;border: none; box-shadow: none;border-collapse: collapse;">
+<table name="tb" width="600" border="1" style="background-color: white;margin: auto;color: rgb(136, 30, 56);font-size: large; font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;border: none; box-shadow: none;border-collapse: collapse;">
   <tr style="background-color: rgb(136, 30, 56);color: white;font-size: large;">
     <th width="100"> <div align="center">Name</div></th>
     <th width="350"> <div align="center">Comment </div></th>
@@ -25,13 +25,26 @@ while($Result = mysqli_fetch_array($res))
   <tr>
     <td><?php echo $Result['Name'];?></div></td>
     <td><?php echo $Result['Comment'];?></td>
-    <td><form action = "delete.php" method = "post" id="CommentForm" style="text-align:left;">
+    <td><form action = "delete.php?Name=$row[0]" method = "post" id="CommentForm" style="text-align:left;">
       <input type="submit" id="btn_del" value="ลบ" style="background-color:  rgb(245, 100, 94);border-radius: 5px;font-size: medium;color: white; height: 25px;border: none;width: 50px;">   
-    <form action = "edit.php" method = "post" id="CommentForm" style="text-align:center;">
+    <form action = "edit.php?Name=$row[0]" method = "post" id="CommentForm" style="text-align:center;">
       <input type="submit" id="btn_edit" value="แก้ไข" style="color:  white;border-radius: 5px;font-size: medium;background-color:rgb(129, 190, 224); height: 25px;border: none;width: 50px;">   
     </form> 
   </tr>
 <?php
+}
+?>
+</table>
+<div class="container" style="text-align: center;padding: 2%;">
+  <a href="https://web247.azurewebsites.net/form.html">
+    <button style="background-color:  rgb(212, 74, 127);border-radius: 5px;font-size: medium;color: white; height: 25px;border: none;width: 50px;">เพิ่ม</button>
+  </a>
+</div>
+<?php
+mysqli_close($conn);
+?>
+</body>
+</html>
 }
 ?>
 </table>
