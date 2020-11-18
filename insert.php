55701fdfc746cfcbeff75add3804b8ea8cff47d5
SupitchayaTammy <73695883+SupitchayaTammy@users.noreply.github.com>
2020-11-18 23:49:03 +0700

    Update insert.php

--- insert.php
+++ insert.php
@@ -1,19 +1,26 @@
-<!DOCTYPE html>
-<html>
-<head>
-</head>
-<body style="background-color: #ffe4e1; font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif; font-size: x-large;">
-  <div class="container">
-    <div class="card" style="background-color: rgb(233, 122, 150);height: 280px; width: 400px;margin: auto;border-radius: 20px; margin-top: 100px;">
-      <div class="card-body" style="color: white;">
-        <form action = "insert.php" method = "post" id="CommentForm" style="text-align:center;">
-        Name<br>
-        <input type="text" name = "name" id="idName" placeholder="Enter Name" style="background-color: #ffe4e1;height: 25px;width: 300px;border-style: solid;"> <br>
-        Comment<br>
-        <textarea rows="10" cols="20" name = "comment" id="idComment" placeholder="Enter Comment" style="background-color: #ffe4e1;height: 140px;width: 300px;border-style: solid;"></textarea><br>
-        <input type="submit" id="commentBtn" style="background-color:  rgb(136, 30, 56);border-radius: 5px;font-size: medium;color: white;border: none;height: 30px;width: 50px;">
-          <button href="https://web247.azurewebsites.net/show.php" style="background-color:  rgb(136, 30, 56);border-radius: 5px;font-size: medium;color: white;border: none;height: 30px;width: 50px;">ย้อนกลับ</button>
-        </form> 
-      </div>
-</body>
-</html>
+  <?php
+
+$conn = mysqli_init();
+mysqli_real_connect($conn, 'supitchaya.mysql.database.azure.com', '_supitchaya_@supitchaya', 'Kmitlitf01', 'itflab', 3306);
+if (mysqli_connect_errno($conn))
+{
+    die('Failed to connect to MySQL: '.mysqli_connect_error());
+}
+
+
+$name = $_POST['name'];
+$comment = $_POST['comment'];
+
+
+
+$sql = "INSERT INTO guestbook (Name , Comment) VALUES ('$name', '$comment')";
+
+if (mysqli_query($conn, $sql)) {
+    echo "New record created successfully";
+    <td><a href="https://web247.azurewebsites.net/show.php">กลับสู่หน้าตาราง</a></td>
+  } else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+  }
+  
+mysqli_close($conn);
+?>
