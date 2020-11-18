fa2336da5dac28b18c0f1671c6d3df7c94f74585
SupitchayaTammy <73695883+SupitchayaTammy@users.noreply.github.com>
2020-11-18 20:51:45 +0700

    Update insert.php

--- insert.php
+++ insert.php
@@ -7,13 +7,20 @@ if (mysqli_connect_errno($conn))
     die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
 
+
 $name = $_POST['name'];
 $comment = $_POST['comment'];
+
+
+
 $sql = "INSERT INTO guestbook (Name , Comment) VALUES ('$name', '$comment')";
+
 if (mysqli_query($conn, $sql)) {
-    echo "Update successful"
+    echo "New record created successfully";
+    echo '<a href="https://web247.azurewebsites.net/show.php">กลับสู่หน้าตาราง</a>';
   } else {
     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
   }
+  
 mysqli_close($conn);
 ?>
