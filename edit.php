dc875fded02051ed7d053e2fb2bd8066167d9b27
SupitchayaTammy <73695883+SupitchayaTammy@users.noreply.github.com>
2020-11-18 22:38:40 +0700

    Update edit.php

--- edit.php
+++ edit.php
@@ -6,33 +6,23 @@ if (mysqli_connect_errno($conn))
 {
     die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
-if (isset($_GET['edit_data'])) {
-		$id = $_GET['edit_data'];
-		$update = true;
-		$record = mysqli_query($conn, "SELECT * FROM guestbook WHERE id=$id");
-
-		if (count($record) == 1 ) {
-			$n = mysqli_fetch_array($record);
-			$name = $n['name'];
-			$commetn = $n['comment'];
-		}
-	}
-<input type="hidden" name="id" value="<?php echo $id; ?>">
-<input type="text" name="name" value="<?php echo $name; ?>">
-<input type="text" name="comment" value="<?php echo $comment; ?>">
-<?php if ($update == true): ?>
-<button class="btn" type="submit" name="update" style="background: #556B2F;" >update</button>
-<?php else: ?>
-<button class="btn" type="submit" name="save" >Save</button>
-<?php endif ?>
-if (isset($_POST['update'])) {
+if(isset($_POST['edit_data'])){
 	$id = $_POST['id'];
 	$name = $_POST['name'];
-	$address = $_POST['comment'];
-
-	mysqli_query($db, "UPDATE info SET name='$name', comment='$comment' WHERE id=$id");
-	$_SESSION['message'] = "Updated!"; 
-	header('location: show.php');
+	$age =  $_POST['age'];	
+	$result = mysqli_query($mysqli, "UPDATE guestbook SET name='$name',comment='$comment' WHERE id=$id");
+	header("Location: show.php");
 }
+}
+?>
+<?php
+$id = $_GET['edit_data'];
+$result = mysqli_query($mysqli, "SELECT * FROM guestbook WHERE id=$id");
+while($Result = mysqli_fetch_array($res))
+{
+$name = $Result['name'];
+$age = $Result['comment'];
+}
+?>
 mysqli_close($conn);
 ?>
