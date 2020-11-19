d92921a8e8f77f65be6200cdf8994b97816392bb
SupitchayaTammy <73695883+SupitchayaTammy@users.noreply.github.com>
2020-11-19 20:42:41 +0700

    Add files via upload

--- update.php
+++ update.php
@@ -9,8 +9,8 @@
 </head>
 <body>
 <?php
-$conn = mysqli_init();
-mysqli_real_connect($conn, 'supitchaya.mysql.database.azure.com', '_supitchaya_@supitchaya', 'Kmitlitf01', 'itflab', 3306);
+$dbconfig = include('dbconfig.php');
+$conn = mysqli_connect($dbconfig['host'], $dbconfig['username'], $dbconfig['password'], $dbconfig['database']);
 if (!$conn)
 {
     die('Failed to connect to MySQL: '.mysqli_connect_error());
@@ -19,12 +19,13 @@ if (!$conn)
 $ID = $_POST['ID'];
 $name = $_POST['name'];
 $text = $_POST['comment'];
-$sql = "UPDATE guestbook SET Name='$name', Comment='$text' WHERE ID='$ID'";
+$link = $_POST['link'];
+$sql = "UPDATE guestbook SET Name='$name', Comment='$text', Link='$link' WHERE ID='$ID'";
 
 if (mysqli_query($conn, $sql)) {
-    echo '<div class="container mt-4">
+    echo '<div class="container">
             <h3>Comment has been updated successfully.</h3>
-            <a role="button" class="btn btn-outline-secondary" href="show.php">Back to Home</a>
+            <a role="button" class="btn btn-primary mt-3" href="guestbook.php">Home</a>
          </div>';
 } else {
     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
