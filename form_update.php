0c233fa6d06703723ecb1b95b0d2999f0f09d129
SupitchayaTammy <73695883+SupitchayaTammy@users.noreply.github.com>
2020-11-19 20:46:03 +0700

    Update form_update.php

--- form_update.php
+++ form_update.php
@@ -11,7 +11,7 @@
 <?php
 $conn = mysqli_init();
 mysqli_real_connect($conn, 'supitchaya.mysql.database.azure.com', '_supitchaya_@supitchaya', 'Kmitlitf01', 'itflab', 3306);
-if (mysqli_connect_errno($conn))
+if (!$conn)
 {
     die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
