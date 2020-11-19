ef3d692e8b33a9592d2f5c61be8f72a5685c581b
SupitchayaTammy <73695883+SupitchayaTammy@users.noreply.github.com>
2020-11-20 06:26:57 +0700

    Update update.php

--- update.php
+++ update.php
@@ -1,13 +1,11 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ITF Database Lab</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/style.css">
+    <title>ITF Lab update</title>
 </head>
-<body>
+<body style="background-color: #ffe4e1;">
 <?php
 $conn = mysqli_init();
 mysqli_real_connect($conn, 'supitchaya.mysql.database.azure.com', '_supitchaya_@supitchaya', 'Kmitlitf01', 'itflab', 3306);
@@ -23,8 +21,8 @@ $sql = "UPDATE guestbook SET Name='$name', Comment='$text' WHERE ID='$ID'";
 
 if (mysqli_query($conn, $sql)) {
     echo '<div class="container">
-            <h3>Comment has been updated successfully.</h3>
-            <a role="button" class="btn btn-primary mt-3" href="show.php">กลับสู่หน้าหลัก</a>
+            <h3 style="color: rgb(136, 30, 56);text-align: center;font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;font-size: x-large;">Comment has been updated successfully.</h3>
+            <button class="btn btn-primary mt-3" href="show.php" style="background-color: rgb(136, 30, 56);color: white;text-align: center;border: hidden;font-size: x-large;font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;">Back to home</button>
          </div>';
 } else {
     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
