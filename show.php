00f8b450a079f54ee64d3ff9fbf8d515d0136c59
SupitchayaTammy <73695883+SupitchayaTammy@users.noreply.github.com>
2020-11-20 06:25:10 +0700

    Update show.php

--- show.php
+++ show.php
@@ -4,21 +4,28 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ITF Lab</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/style.css">
+<style>
+    body {background-color: #ffe4e1;text-align: center;color:rgb(136, 30, 56);font-size: larger ;font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;}
+    table {border-collapse: collapse;margin: auto;background-color: white;}
+    th {background-color:rgb(136, 30, 56);color: white;height: 40px;font-size: 22px;width: 400px;text-align: center;}
+    tr:nth-child(even){background-color: #ffccff;}
+    .btn_edit {background-color: tomato;border: hidden; font-size: large; color: white;width: 62px;}
+    .btn_del {background-color: rgb(113, 173, 230);border: hidden; font-size: large; color: white;width: 62px;}
+    .btn_add {background-color :rgb(136, 30, 56);margin-top: auto;border: hidden; font-size: large; color: white;width: 55px;}
+</style>
 </head>
 <body>
 <?php
 $conn = mysqli_init();
 mysqli_real_connect($conn, 'supitchaya.mysql.database.azure.com', '_supitchaya_@supitchaya', 'Kmitlitf01', 'itflab', 3306);
-if (!$conn)
+if (mysqli_connect_errno($conn))
 {
     die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
 $res = mysqli_query($conn, 'SELECT * FROM guestbook');
 ?>
 <div class="container">
-    <h1>Table of guestbook</h1>
+    <h1>Guestbook</h1>
     <table class="table table-responsive-md">
         <thead>
             <tr>
@@ -39,11 +46,10 @@ while($row = mysqli_fetch_array($res))
                     <div class="d-inline">
                         <form action="form_update.php" method="post" class="d-inline">
                             <input type="hidden" name="ID" value=<?php echo $row['ID'];?>>
-                            <button type="submit" class="btn btn-sm btn-primary mb-1">แก้ไข</button>
-                        </form>
+                            <button type="submit" class="btn_edit">Edit</button>
                         <form action="delete.php" method="post" class="d-inline">
                             <input type="hidden" name="ID" value=<?php echo $row['ID'];?>>
-                            <button type="submit" class="btn btn-sm btn-danger mb-1">ลบ</button>
+                            <button type="submit" class="btn_del">Delete</button>
                         </form>
                     </div>
                 </td>
@@ -55,7 +61,7 @@ mysqli_close($conn);
 ?>
     </table>
     <div class="text-center">
-        <a role='button' href="form.php" class="btn btn-primary">เพิ่ม</button></a>
+        <button href="form.php" class="btn_add">Add</button>
     </div>
 </div>
 </body>
