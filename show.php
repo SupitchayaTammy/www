f066a04d4a8671682b5e4ea075b13b27ae4a7741
SupitchayaTammy <73695883+SupitchayaTammy@users.noreply.github.com>
2020-11-20 09:45:59 +0700

    Update show.php

--- show.php
+++ show.php
@@ -8,10 +8,9 @@
     body {background-color: #ffe4e1;text-align: center;color:rgb(136, 30, 56);font-size: larger ;font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;}
     table {border-collapse: collapse;margin: auto;background-color: white;}
     th {background-color:rgb(136, 30, 56);color: white;height: 40px;font-size: 22px;width: 400px;text-align: center;}
-    tr:nth-child(odd){background-color: #ffccff;}
     .btn-danger {background-color: tomato;border: hidden; font-size: large; color: white;width: 62px;}
     .btn-primary {background-color: rgb(113, 173, 230);border: hidden; font-size: large; color: white;width: 62px;}
-    .bth-show {background-color: rgb(136, 30, 56);border: hidden; font-size: large; color: white;width: 62px;}
+    .btn-show {background-color: rgb(136, 30, 56);border: hidden; font-size: large; color: white;width: 62px;}
 </style>
 </head>
 <body>
