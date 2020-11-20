c1e3ebdda4cf15f70661dc322bb7bd3b06a9fcdc
SupitchayaTammy <73695883+SupitchayaTammy@users.noreply.github.com>
2020-11-20 13:16:26 +0700

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,3 +1,16 @@
+!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ITF Database Lab</title>
+<style>
+    body {background-color: #ffe4e1;text-align: center;color:rgb(136, 30, 56);font-size: larger ;font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;}
+    h3 {color: rgb(136, 30, 56);font-size:larger}
+    .btn-dl {background-color: rgb(136, 30, 56);border: hidden; font-size: x-large; color: white}
+</style>
+</head>
+<body>
 <?php
 
 $conn = mysqli_init();
@@ -10,8 +23,8 @@ $ID = $_POST['ID'];
 $sql = "DELETE FROM guestbook WHERE ID='$ID'";
 if (mysqli_query($conn, $sql)) {
     echo '<div class="container">
-            <h3 style="color: rgb(136, 30, 56);font-size=large">Comment has been deleted successfully.</h3>
-            <a href="show.php"><button class="btn btn-primary mt-3" style="background-color: rgb(136, 30, 56);border: hidden; font-size: x-large; color: white">Back to home</button></a>
+            <h3">Comment has been deleted successfully.</h3>
+            <a href="show.php"><button class="btn-dl">Back to home</button></a>
          </div>';
 } else {
     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
