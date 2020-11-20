b5ec00ae546b44d013230a47ee9949506293bda8
SupitchayaTammy <73695883+SupitchayaTammy@users.noreply.github.com>
2020-11-20 08:34:48 +0700

    Update update.php

--- update.php
+++ update.php
@@ -22,7 +22,7 @@ $sql = "UPDATE guestbook SET Name='$name', Comment='$text' WHERE ID='$ID'";
 if (mysqli_query($conn, $sql)) {
     echo '<div class="container">
             <h3>Comment has been updated successfully.</h3>
-            <a role="button" style="background-color=rgb(136, 30, 56);" class="btn btn-primary mt-3" href="show.php">Back to home</a>
+            <a style="background-color=rgb(136, 30, 56);" class="btn btn-primary mt-3" href="show.php"><button>Back to home</a>
          </div>';
 } else {
     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
