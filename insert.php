3c680de32d01021991620c868691ab2d55d67ae2
SupitchayaTammy <73695883+SupitchayaTammy@users.noreply.github.com>
2020-11-20 08:55:15 +0700

    Update insert.php

--- insert.php
+++ insert.php
@@ -3,16 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Insert</title>
-<style>
-    body {background-color: #ffe4e1;"}
-    h {color= rgb(136, 30, 56);font-size=x-large;font-family='Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;"}
-    td {background-color: rgb(136, 30, 56);color: white;border: hidden;font-size: x-large;font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;"}
-</style>
+    <title>ITF Add/title>
 </head>
 <body>
-<<?php
-
+<?php
 $conn = mysqli_init();
 mysqli_real_connect($conn, 'supitchaya.mysql.database.azure.com', '_supitchaya_@supitchaya', 'Kmitlitf01', 'itflab', 3306);
 if (mysqli_connect_errno($conn))
@@ -20,22 +14,20 @@ if (mysqli_connect_errno($conn))
     die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
 
-
 $name = $_POST['name'];
 $comment = $_POST['comment'];
-
-
-
 $sql = "INSERT INTO guestbook (Name , Comment) VALUES ('$name', '$comment')";
 
 if (mysqli_query($conn, $sql)) {
-    echo "New record created successfully";
-    <a class="btn btn-add" href="show.php"><button>Back to home</button></a>
-  } else {
+    echo '<div class="container">
+            <h3>Comment has been saved in to the database successfully.</h3>
+            <a role="button" class="btn btn-primary mt-3" href="show.php" style='background-color: rgb(136, 30, 56);color: white'>Back to home</a>
+         </div>';
+} else {
     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-  }
+}
   
 mysqli_close($conn);
 ?>
-<\body>
-<\html>
+</body>
+</html>
