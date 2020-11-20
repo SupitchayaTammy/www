29c980c9cfbc7946030c880951f60e6c821388f1
SupitchayaTammy <73695883+SupitchayaTammy@users.noreply.github.com>
2020-11-20 09:14:39 +0700

    Update form_update.php

--- form_update.php
+++ form_update.php
@@ -6,7 +6,7 @@
     <title>Edit table</title>
 <style>
     body {background-color: #ffe4e1;text-align: center;color:rgb(136, 30, 56);font-size: larger ;font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;}
-    .form-group {font-size: larger;}
+    .form-group {font-size: x-large;}
     .btn-save {background-color: rgb(136, 30, 56);color: white;text-align: center;border: hidden;font-size: large;}
     .btn-home {background-color: rgb(136, 30, 56);color: white;text-align: center;border: hidden;font-size: large;}
 </style>    
