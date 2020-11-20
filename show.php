69d399762f8e62a9aae980ee4756232be9327db6
SupitchayaTammy <73695883+SupitchayaTammy@users.noreply.github.com>
2020-11-20 09:38:10 +0700

    Update show.php

--- show.php
+++ show.php
@@ -61,7 +61,7 @@ mysqli_close($conn);
 ?>
     </table>
     <div class="text-center">
-         <a class="btn-show" href="form.php"><button style='background-color=rgb(136, 30, 56),color= white'>Add</button></a>
+         <a class="btn-show" href="form.php" style='background-color=rgb(136, 30, 56),color= white><button>Add</button></a>
     </div>
 </div>
 </body>
