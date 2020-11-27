3887e4f0beb07ef572e238ec25b20400bafab48e
SupitchayaTammy <73695883+SupitchayaTammy@users.noreply.github.com>
2020-11-27 17:09:18 +0700

    Create update_form.php

---
+++ update_form.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Edit table</title>
+<style>
+    body {background-color: #ffe4e1;text-align: center;color:rgb(136, 30, 56);font-size: xx-large ;font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;}
+    .form-group {font-size: x-large;}
+    .btn-save {background-color: rgb(136, 30, 56);color: white;text-align: center;border: hidden;font-size:x-large}
+</style>    
+</head>
+<body>
+<?php
+$conn = mysqli_init();
+mysqli_real_connect($conn, 'supitchaya247.mysql.database.azure.com', 'supitchaya247@supitchaya247', 'Labitftest01', 'itftest', 3306);
+if (mysqli_connect_errno($conn))
+{
+    die('Failed to connect to MySQL:'.mysqli_connect_error());
+}
+
+$id = $_POST['id'];
+$sql = "SELECT * FROM bmi WHERE id='$id'";
+$res = mysqli_query($conn, $sql);
+$result = mysqli_fetch_array($res);
+?>
+    <div class="container mt-4">
+        <h1>Edit</h1>
+        <form action="update.php" method="post" class="mt-4">
+            <input type="hidden" name="id" value=<?php echo $result['id'];?>>
+            <div class="form-group">
+                <label for="inputName">Name</label><br>
+                <?php
+                    echo '<input type="text" name="weight" id="inputWeight" class="form-control" placeholder="Update Weight" value="'.$result["weight"].'">'
+                ?>
+            </div>
+            <div class="form-group">
+                <label for="inputComment">Comment</label><br>
+                <textarea name="height" class="form-control" id="inputHeight" placeholder="Update Height"><?php echo $result['height'];?></textarea>
+            </div>
+            </div>
+            <div class="mt-4">
+                <button type="submit" class="btn-save">Save</button> 
+            </div>
+        </form>
+    </div>
+<?php
+mysqli_close($conn);
+?>
+</body>
+</html>
