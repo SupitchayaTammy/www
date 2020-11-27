a1cdf516daa72c53f698b74492e7d12e99c1c51d
SupitchayaTammy <73695883+SupitchayaTammy@users.noreply.github.com>
2020-11-27 16:45:51 +0700

    Update insertex.php

--- insertex.php
+++ insertex.php
@@ -1,21 +1,25 @@
 <?php
 $conn = mysqli_init();
-mysqli_real_connect($conn, 'labitfkmitl.mysql.database.azure.com', 'supitchaya@labitfkmitl', 'Labitfkmitl03', 'table_abc', 3306);
+mysqli_real_connect($conn, 'supitchaya247.mysql.database.azure.com', 'supitchaya247@supitchaya247', 'Labitftest01', 'itftest', 3306);
 if (mysqli_connect_errno($conn))
 {
     die('Failed to connect to MySQL:'.mysqli_connect_error());
 }
 
-$A = $_POST['A'];
-$B = $_POST['B'];
+$name = $_POST['name'];
+$height = $_POST['height'];
+$weight = $_POST['weight'];
 
-$sql = "INSERT INTO abc (A , B) VALUES ('$A', '$B')";
+$sql = "INSERT INTO bmi (name , height , weight) VALUES ('$name', '$height', '$weight')";
 
 if (mysqli_query($conn, $sql)) {
-    echo 'Successfully';
-    } else {
+    echo '<div class="container">
+            <h3>Saved successfully.</h3>
+            <a href="showform.php"><bottom class="btn-insert">Back to home</a>
+         </div>';
+} else {
     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-    }
+}
   
 mysqli_close($conn);
 ?>
