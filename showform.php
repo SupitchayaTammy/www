999ac28e9957452245320a53afb936a020405199
SupitchayaTammy <73695883+SupitchayaTammy@users.noreply.github.com>
2020-11-27 17:12:55 +0700

    Update showform.php

--- showform.php
+++ showform.php
@@ -28,7 +28,7 @@ while($Result = mysqli_fetch_array($res))
       <td><?php echo $Result['name'];?></div></td>
       <td><?php echo $Result['weight'];?></td>
       <td><?php echo $Result['height'];?></td>
-     <td><?php echo $Result['weight'] / ($Result['height'] * $Result['height']);?></td>
+     <td><?php echo $Result['weight'] / (($Result['height']/100) * ($Result['height']/100));?></td>
       <form action="update_form.php" method="post">
         <input type="hidden" name="id" value=<?php echo $row['id'];?>>
         <button type="submit">update</button>
